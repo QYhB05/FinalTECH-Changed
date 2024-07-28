ee5c90490dfd6c2f1cffe65646346dc51cc92d84
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2024-07-28 13:48:38 +0800

    Delete src/main/java/io/taraxacum/finaltech/interfaces directory

--- src/main/java/io/taraxacum/finaltech/interfaces/UnCopiableItem.java
+++
@@ -1,9 +0,0 @@
-package io.taraxacum.finaltech.core.interfaces;
-
-/**
- * @author Final_ROOT
- * @see io.taraxacum.finaltech.core.item.machine.operation.ItemSerializationConstructor
- * @since 2.2
- */
-public interface UnCopiableItem {
-}
