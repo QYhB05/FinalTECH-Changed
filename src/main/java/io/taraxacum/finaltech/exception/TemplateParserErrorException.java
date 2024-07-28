d73b2f70c97e842ce68cf59720817b1b97ffdd16
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2024-07-28 13:49:41 +0800

    Delete src/main/java/io/taraxacum/finaltech/exception directory

--- src/main/java/io/taraxacum/finaltech/exception/TemplateParserErrorException.java
+++
@@ -1,8 +0,0 @@
-package io.taraxacum.finaltech.core.exception;
-
-public class TemplateParserErrorException extends Exception {
-
-    public TemplateParserErrorException(String message) {
-        super(message);
-    }
-}
